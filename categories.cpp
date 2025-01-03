c920b6a7840c9d385013b89027847eeb931424b3
NVBalandin22 <132457025+NVBalandin22@users.noreply.github.com>
2025-01-03 16:13:49 +0200

    Return button fix, design improvements

--- categories.cpp
+++ categories.cpp
@@ -1,18 +1,23 @@
 #include "categories.h"
 #include "ui_categories.h"
+//#include <thread>
+//#include <chrono>
 categories::categories(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::categories)
 {
     ui->setupUi(this);
+    ui->animalCardWidget->hide();
     QPixmap pix(":/images/img/logo.png");
-    ui->label_4->setPixmap(pix);
+    ui->LogoLabel->setPixmap(pix);
     setWindowTitle("Info");
-    ui->SearchLineCat_2->setPlaceholderText("Search");
-    //QAction *searchAction = new QAction(QIcon(":/images/img/search.jpg"), "", this);
-    //ui->SearchLine->addAction(searchAction, QLineEdit::LeadingPosition);
+    ui->SearchLineCat->setPlaceholderText("Search");
+    QAction *searchAction = new QAction(QIcon(":/images/img/betterSearch.jpg"), "", this);
+    ui->SearchLineCat->addAction(searchAction, QLineEdit::LeadingPosition);
+    //QPixmap pix2(":/images/img/betterSearch.jpg");
+    //ui->searchIcon->setPixmap(pix2);
 }
-
+int a = 1;
 categories::~categories()
 {
     delete ui;
@@ -28,7 +33,20 @@ void categories::on_returnButton_clicked()
 
 void categories::on_treeWidget_2_itemDoubleClicked(QTreeWidgetItem *item, int column)
 {
-    QString ITEMtext = item->text(column);
+    if(a == 1){
+        QString ITEMtext = item->text(column);
+        ui->animalNameLabel->setText(ITEMtext);
+        ui->animalCardWidget->show();
+        a++;
+    }
+    else{
+        ui->animalCardWidget->hide();
+        //std::this_thread::sleep_for(std::chrono::seconds(1));
+        QString ITEMtext = item->text(column);
+        ui->animalNameLabel->setText(ITEMtext);
+        ui->animalCardWidget->show();
+    }
+
 }
 
 
@@ -36,4 +54,7 @@ void categories::on_animalCardCloseButton_clicked()
 {
     ui->animalCardWidget->hide();
 }
+
+
+
 
